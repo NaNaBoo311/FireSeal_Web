90bd111ec5004ad9731dd3703e2a384416aafecf
NaNaBoo311 <gtacbt0@gmail.com>
2025-09-25 13:13:24 +0700

    fix deploy problems

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import "./App.css";
 import EmailConfirm from "./pages/EmailConfirm";
 import ResetPassword from "./pages/ResetPassword";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 function App() {
   return (
-    <Router>
-      {" "}
-      <Routes>
-        {" "}
-        <Route path="/" element={<EmailConfirm />} />{" "}
-        <Route path="/reset" element={<ResetPassword />} />{" "}
-      </Routes>{" "}
-    </Router>
+    <Routes>
+      <Route path="/" element={<EmailConfirm />} />
+      <Route path="/reset" element={<ResetPassword />} />
+    </Routes>
   );
 }
+
 export default App;
